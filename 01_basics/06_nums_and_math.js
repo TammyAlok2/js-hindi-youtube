3c51ad1a0393b8142a7e5a8f9683bcb5f704139e
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2024-03-14 05:01:40 +0000

    Math and number video completed

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,34 +1,35 @@
+/*
 const score = 400
-// console.log(score);
+console.log(score)
+const balance = new Number (100);
 
-const balance = new Number(100)
-// console.log(balance);
+console.log(balance)
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
+console.log(balance.toString().length);
 
-const otherNumber = 123.8966
+console.log(balance.toFixed(1))
+const otherNumber= 1123.128;
+console.log(otherNumber.toPrecision(3))
 
-// console.log(otherNumber.toPrecision(4));
+// local string 
+const hundreds= 100000;
+console.log(hundreds.toLocaleString('en-In'))
 
-const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
+//++++++++++++++++++++++++++++Maths ++++
+*/
 
-// +++++++++++++ Maths +++++++++++++++++++++++++++++
+console.log(Math) // it is object 
+console.log(Math.abs(-8)) //8
+console.log(Math.round(4.6)) //5
+console.log(Math.ceil(4.2)) // 5
+console.log(Math.floor(4.2)) //4
+console.log(Math.min(4,3,44,5))
+console.log(Math.max(3,2, 5,2345234))
+console.log(Math.random())
+console.log(Math.round(Math.random()*10)+1) // taking random value 
 
-// console.log(Math);
-// console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
-// console.log(Math.min(4, 3, 6, 8));
-// console.log(Math.max(4, 3, 6, 8));
+const min =10;
+const max = 20;
+// to find number between the range
+console.log(Math.floor(Math.random() * (max-min +1) )+min) 
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
-
-const min = 10
-const max = 20
-
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
