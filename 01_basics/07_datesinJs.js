0eda27171215992a80dc98d0738fe1e694cd52c8
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2024-03-14 05:25:48 +0000

    time and date paritially completed

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,32 +1,21 @@
-// Dates
+console.log(Date) //function
 
-let myDate = new Date()
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+let myDate =new Date();
+console.log(myDate.getDate()) // gives only date
+console.log(myDate.toString()) // gives whole output in string
+console.log(myDate.toLocaleDateString()) // 03/14/2024
+console.log(myDate.toDateString()) //Thu Mar 14 2024
+console.log(typeof myDate) //object
 
-// let myCreatedDate = new Date(2023, 0, 23)
-// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
-// let myCreatedDate = new Date("2023-01-14")
-let myCreatedDate = new Date("01-14-2023")
-// console.log(myCreatedDate.toLocaleString());
+let myCreatedDate = new Date (2023,0,23,7,9);
+console.log(myCreatedDate.toDateString())// IN js month started from 0 
 
-let myTimeStamp = Date.now()
+let myTimeStamps = Date.now()
+console.log(myTimeStamps)
 
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
-
-let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
-
-// `${newDate.getDay()} and the time `
-
-newDate.toLocaleString('default', {
-    weekday: "long",
+let newDate = Date.now()
+let non= newDate.toLocaleString('default',{
+    weekDay:"long",
     
 })
-
+console.log(non)
