e7fb0c75816cdcba20edd9f684545b766886e1d4
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2024-03-16 18:07:29 +0000

    object completed succesfully

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,38 +1,43 @@
-// singleton
-// Object.create
-
+console.log('this is working ')
+// singleton 
+// it is constructor method to make the object 
+const obj1 = Object.create({hello:'this'});
+console.log(obj1);
 // object literals
 
 const mySym = Symbol("key1")
+const mySym1 = Symbol("key2")
+ const JsUser = {
+    name:"alok tamrakar ",
+   mySym :'key1',
+   [mySym1] :"mykey1",
+    age :18,
+    location:"Jaipur",
+    email:'aloktamrakar2@gmail.com',
+    lastLogin:['monday','tuesdaay']
+ }
+//console.log(JsUser."mylove")
+ //console.log(JsUser[email])// error
+ console.log(JsUser['email'])
+ console.log(JsUser[mySym1])
+ console.log(typeof
+    (JsUser.mySym)) // string 
+    console.log(typeof(JsUser.mySym1)) // undefined 
+    console.log(typeof(JsUser[mySym1]))//string correct syntax 
 
+    
+   // Object.freeze(JsUser);// cannot change the object 
+    JsUser.email ='aloktamraar'
+    console.log(JsUser)
 
-const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary",
-    [mySym]: "mykey1",
-    age: 18,
-    location: "Jaipur",
-    email: "hitesh@google.com",
-    isLoggedIn: false,
-    lastLoginDays: ["Monday", "Saturday"]
-}
-
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
-// console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
-
-JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
-// console.log(JsUser);
-
-JsUser.greeting = function(){
-    console.log("Hello JS user");
-}
-JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
-}
-
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+    // function in object 
+    JsUser.greeting = function(){
+        console.log('hello good morning')
+    }
+    JsUser.greeting2 = function (){
+        console.log(`Hello js user ${this.name}`)
+        // `${}` string interpolation 
+    }
+    console.log(JsUser.greeting2())
+    console.log(JsUser.greeting);// undefined 
+    console.log(JsUser.greeting())
