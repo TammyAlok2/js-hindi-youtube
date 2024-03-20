99cb8eb25565ea9c8a25d082049cc625fefbc210
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2024-03-20 13:32:12 +0000

    IIFE completed successfully

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,12 +1,27 @@
-// Immediately Invoked Function Expressions (IIFE)
-
-
-(function chai(){
-    // named IIFE
-    console.log(`DB CONNECTED`);
+// Immediately Invoked function Expressions (IIFE)
+// ()() this is IIFE to immediate execute the functio 
+// global scope ke pollution ko hatane ke liye IIFE use karte hai 
+// semicolon is very important in IIFE __________________very important 
+(function chai (){
+    // named IIFE 
+    console.log("Db connected successfully ");
 })();
 
-( (name) => {
-    console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
+(()=>{
+    console.log('Db connected 2 sucess');
+}) ();
 
+
+((name)=>{
+console.log(`Heloo ${name}`);
+})("alok");
+
+(function hello (){
+    console.log("hello this is me 1 ")
+})();
+(()=>{
+    console.log("hello i am alok tamrakr ")
+})();
+((name)=>{
+    console.log(`Hello everyone I am ${name}`)
+})("rimjhim");
